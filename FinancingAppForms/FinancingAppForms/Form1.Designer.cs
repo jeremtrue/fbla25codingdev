504d8fde9ba62fc572c91e4e02df89bd2c2955e2
MrElectorium <112230327+MrElectorium@users.noreply.github.com>
2025-01-04 22:31:31 -0700

    Add files via upload

---
+++ FinancingAppForms/FinancingAppForms/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace FinancingAppForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelNav = new System.Windows.Forms.Panel();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnStatistics = new System.Windows.Forms.Button();
+            this.btnSpending = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.panelNav);
+            this.panel1.Controls.Add(this.btnSettings);
+            this.panel1.Controls.Add(this.btnStatistics);
+            this.panel1.Controls.Add(this.btnSpending);
+            this.panel1.Controls.Add(this.btnDashboard);
+            this.panel1.Controls.Add(this.pictureBoxLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(146, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // panelNav
+            // 
+            this.panelNav.BackColor = System.Drawing.Color.White;
+            this.panelNav.Location = new System.Drawing.Point(0, 193);
+            this.panelNav.Name = "panelNav";
+            this.panelNav.Size = new System.Drawing.Size(3, 100);
+            this.panelNav.TabIndex = 5;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Apercu Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.ForeColor = System.Drawing.Color.Transparent;
+            this.btnSettings.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSettings.ImageIndex = 3;
+            this.btnSettings.ImageList = this.imageList1;
+            this.btnSettings.Location = new System.Drawing.Point(0, 285);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(146, 55);
+            this.btnSettings.TabIndex = 4;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click_1);
+            this.btnSettings.Leave += new System.EventHandler(this.btnSettings_Leave);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Dashboard.png");
+            this.imageList1.Images.SetKeyName(1, "Spending.png");
+            this.imageList1.Images.SetKeyName(2, "Statistics.png");
+            this.imageList1.Images.SetKeyName(3, "Settings.png");
+            this.imageList1.Images.SetKeyName(4, "Close Button.png");
+            // 
+            // btnStatistics
+            // 
+            this.btnStatistics.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnStatistics.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStatistics.FlatAppearance.BorderSize = 0;
+            this.btnStatistics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStatistics.Font = new System.Drawing.Font("Apercu Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStatistics.ForeColor = System.Drawing.Color.Transparent;
+            this.btnStatistics.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnStatistics.ImageIndex = 2;
+            this.btnStatistics.ImageList = this.imageList1;
+            this.btnStatistics.Location = new System.Drawing.Point(0, 230);
+            this.btnStatistics.Name = "btnStatistics";
+            this.btnStatistics.Size = new System.Drawing.Size(146, 55);
+            this.btnStatistics.TabIndex = 3;
+            this.btnStatistics.Text = "Statistics";
+            this.btnStatistics.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStatistics.UseVisualStyleBackColor = true;
+            this.btnStatistics.Click += new System.EventHandler(this.btnStatistics_Click);
+            this.btnStatistics.Leave += new System.EventHandler(this.btnStatistics_Leave);
+            // 
+            // btnSpending
+            // 
+            this.btnSpending.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSpending.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSpending.FlatAppearance.BorderSize = 0;
+            this.btnSpending.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSpending.Font = new System.Drawing.Font("Apercu Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpending.ForeColor = System.Drawing.Color.Transparent;
+            this.btnSpending.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSpending.ImageIndex = 1;
+            this.btnSpending.ImageList = this.imageList1;
+            this.btnSpending.Location = new System.Drawing.Point(0, 175);
+            this.btnSpending.Name = "btnSpending";
+            this.btnSpending.Size = new System.Drawing.Size(146, 55);
+            this.btnSpending.TabIndex = 2;
+            this.btnSpending.Text = "Spending";
+            this.btnSpending.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSpending.UseVisualStyleBackColor = true;
+            this.btnSpending.Click += new System.EventHandler(this.btnSpending_Click);
+            this.btnSpending.Leave += new System.EventHandler(this.btnSpending_Leave);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnDashboard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Apercu Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.Transparent;
+            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDashboard.ImageIndex = 0;
+            this.btnDashboard.ImageList = this.imageList1;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 120);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(146, 55);
+            this.btnDashboard.TabIndex = 1;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            this.btnDashboard.Leave += new System.EventHandler(this.btnDashboard_Leave);
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo.Image")));
+            this.pictureBoxLogo.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(146, 120);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo.TabIndex = 0;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Button btnSpending;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnStatistics;
+        private System.Windows.Forms.Panel panelNav;
+    }
+}
+
---
+++ FinancingAppForms/FinancingAppForms/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace FinancingAppForms
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+            panelNav.Height = btnDashboard.Height;
+            panelNav.Top = btnDashboard.Top;
+            panelNav.Left = btnDashboard.Left;
+            btnDashboard.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            panelNav.Height = btnDashboard.Height;
+            panelNav.Top = btnDashboard.Top;
+            panelNav.Left = btnDashboard.Left;
+            btnDashboard.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnSpending_Click(object sender, EventArgs e)
+        {
+            panelNav.Height = btnSpending.Height;
+            panelNav.Top = btnSpending.Top;
+            panelNav.Left = btnSpending.Left;
+            btnSpending.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnStatistics_Click(object sender, EventArgs e)
+        {
+            panelNav.Height = btnStatistics.Height;
+            panelNav.Top = btnStatistics.Top;
+            panelNav.Left = btnStatistics.Left;
+            btnStatistics.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnSettings_Click_1(object sender, EventArgs e)
+        {
+            panelNav.Height = btnSettings.Height;
+            panelNav.Top = btnSettings.Top;
+            panelNav.Left = btnSettings.Left;
+            btnSettings.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnDashboard_Leave(object sender, EventArgs e)
+        {
+            btnDashboard.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSpending_Leave(object sender, EventArgs e)
+        {
+            btnSpending.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSettings_Leave(object sender, EventArgs e)
+        {
+            btnSettings.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnStatistics_Leave(object sender, EventArgs e)
+        {
+            btnStatistics.BackColor = Color.FromArgb(24, 30, 54);
+        }
+    }
+}
